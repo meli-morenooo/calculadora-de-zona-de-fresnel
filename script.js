426bff7b191654589ed0e8d656ac587a0e0004db
Melisa Moreno <meli.moreno419@gmail.com>
2023-08-22 17:58:42 -0300

    Ajuste de prioridad de calculo

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ document.addEventListener('DOMContentLoaded', function () {
             contenedorResultado.style.display = 'none';
             mensajeError.style.display = 'block';
         } else {
-            const F1 = 8.656 * Math.sqrt(distanciaEntrada) / frecuenciaEntrada;
+            const F1 = 8.656 * Math.sqrt(distanciaEntrada / frecuenciaEntrada);
             mensajeError.style.display = 'none';
             resultadoFresnel.textContent = `F1: ${F1.toFixed(2)} metros`;
             tablaPasos.style.display = 'block';
             valorDistancia.textContent = distanciaEntrada;
             valorFrecuencia.textContent = frecuenciaEntrada;
-            const pasoCalculo = `F1[m] = 8.656 * √(${distanciaEntrada}) / ${frecuenciaEntrada.toFixed(2)} ≈ ${F1.toFixed(3)} metros`;
+            const pasoCalculo = `F1[m] = 8.656 * √(${distanciaEntrada} / ${frecuenciaEntrada.toFixed(2)}) ≈ ${F1.toFixed(3)} metros`;
             document.getElementById('paso-calculo').textContent = pasoCalculo;
             contenedorResultado.style.display = 'block';
         }
